a8b7489f6e9d528a67e40b1fb3e4a370fc971f94
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-10-15 13:04:22 +0200

    make the app run using Flask

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 # We are impoting Flask class from flask package
-from flask import flask
+from flask import Flask
 
 #  This is an instance of using this class and storing it in a variable called app.
 #  The first arg is the name of the application's module - our package.
@@ -20,13 +20,14 @@ def index():
 
 # The name "__main__" is the name of default module in Python.
 # This is the first one that we run, so if it has not been imported, it will be run directly.
+# We get the IP environment if it exist and set a default if not found.
+#  5000 is common port used by Flask.
+# We should never have the debug ==true in a production app or when we submit projects for assessmnet.
+# This can allow arbitary code to be run and then is a security flaw. we can have it true only to test 
+#  the app or to debug.
+
 if __name__ == "__main__":
     app.run(
-        # We get the
-         IP environment if it exist and set a default if not found.
-        #  5000 is common port used by Flask.
-        # 
         host = os.environ.get("IP", "0.0.0.0"),
         port = int(os.environ.get("PORT", "5000")),
-        debug = True
-    )
+        debug = True)
