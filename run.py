c0f7473ce66581fc1577ebcd6fbe69d09300c8df
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-10-18 13:57:41 +0200

    display data from company.json into html

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 # We are impoting Flask class from flask package
 from flask import Flask, render_template
 
@@ -18,16 +19,19 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")    
+    data =[]
+    with open("data/company.json", "r") as json_data:
+        data= json.load(json_data)
+    return render_template("about.html", page_title="About", company = data)    
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")   
+    return render_template("contact.html", page_title="Contact")   
 
 
 @app.route("/career")
 def career():
-    return render_template("career.html") 
+    return render_template("career.html", page_title="Careers") 
 
 
 
