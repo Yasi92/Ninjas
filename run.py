2c2f2e2b78bbfc0a3a912a63c50a4ce7050e66b9
Yasi <79868623+Yasi92@users.noreply.github.com>
2021-10-18 15:00:05 +0200

    create member.html and set advanced routing for each member to display in member.html file

--- run.py
+++ run.py
@@ -24,6 +24,25 @@ def about():
         data= json.load(json_data)
     return render_template("about.html", page_title="About", company = data)    
 
+
+"""
+the member_name here in angle brackets will be passed into the view.
+meaning that everythin that comes after /about/ will be passed as an arg into the view named about_member.
+"""
+
+# If the url of the obj is equal to the name that comes after /about/ (member_name), then turn that object into a dict called member.
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)    
+        for obj in data :
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)            
+
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")   
